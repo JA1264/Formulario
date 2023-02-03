658b1608943556898d35a04fc43f81b1ce117315
Jorge Arturo <221251@ids.upchiapas.edu.mx>
2023-02-02 22:59:48 -0600

    Actualizacion2.1

--- src/components/atoms/FormRegister.jsx
+++ src/components/atoms/FormRegister.jsx
@@ -47,6 +47,7 @@ function FormRegister() {
             </div>
             <button onClick={handlerClick}>Registro</button>
             <Link to='/login'><button>Regresar</button></Link>
+            <h1>ndjksjdscsdjcdjlcdklcd</h1>
            <div>
                
            </div>
